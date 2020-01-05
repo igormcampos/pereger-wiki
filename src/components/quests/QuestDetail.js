549ceab9f7346321ef6477c7bc5e66a908f27730
Igor Meurer Campos <igorcampos7@gmail.com>
2020-01-04 22:05:19 -0300

    Fix quest requirements and rewards links

--- src/components/quests/QuestDetail.js
+++ src/components/quests/QuestDetail.js
@@ -6,21 +6,23 @@ class QuestDetail extends React.Component {
     render() {
         if (this.props.quest) {
             const {title, desc, target, amount, reward, rewardAmt, objective} = this.props.quest;
+            const requirementsLink = <p>Requirements: {target && amount ?
+                // objective 0; quest = 1; talk = 2; kill = 3; action = 4
+                objective === 2 ?
+                    <Link to={'/monsters/' + target.name}>{'Talk to ' + target.name}</Link>
+                    : objective === 3 ?
+                    <Link to={'/monsters/' + target.className}>{'Kill ' + amount + ' ' + target.name}</Link>
+                    : <Link to={target.category === 10 ? '/items/' + target.className : '/items/' + target.name}>{target.className + ' x ' + amount}</Link>
+                : ''}
+            </p>;
+            const rewardLink = reward && rewardAmt && <Link to={reward.category === 10 ? '/items/' + reward.className : '/items/' + reward.name}>{reward.className}</Link>;
 
             return (
                 <div>
                     <h4>{title}</h4>
                     <p>{desc}</p>
-                    <p>Requirements: {target && amount ?
-                        // objective 0; quest = 1; talk = 2; kill = 3; action = 4
-                        objective === 2 ?
-                            <Link to={'/monsters/' + target.name}>{'Talk to ' + target.name}</Link>
-                        : objective === 3 ?
-                            <Link to={'/monsters/' + target.className}>{'Kill ' + amount + ' ' + target.name}</Link>
-                        : <Link to={target.category === 10 ? '/items/' + target.className : '/items/' + target.name}>{target.className + ' x ' + amount}</Link>
-                    : ''}
-                    </p>
-                    <p>Rewards: {reward && rewardAmt && <Link to={reward.category === 10 ? '/items/' + reward.className : '/items/' + reward.name}>{reward.className}</Link> + ' x ' + rewardAmt}</p>
+                    {target && amount && requirementsLink}
+                    {rewardLink && <p>Rewards: {rewardLink} x {rewardAmt}</p>}
                 </div>
             )
         }
